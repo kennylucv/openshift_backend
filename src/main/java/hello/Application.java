bc15574f8e225037ef910ecf318a8e0d059eb54f
kenny luc <kennylucv@gmail.com>
2018-01-18 17:16:34 +0000

    removed not needed dependencies

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,7 +1,6 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.*;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,6 +1,5 @@
 package hello;
 
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import java.util.ArrayList;
 
